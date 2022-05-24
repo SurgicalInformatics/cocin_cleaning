e5dae49e9de00e225cb4adeabd207ab34a1db99c
riinuots <riinuots@gmail.com>
2022-05-24 14:29:22 +0100

    add in number_symptoms and any_symptoms variables

--- 01_oneline.R
+++ 01_oneline.R
@@ -24,18 +24,62 @@ count(oneline, dexamethasone.yn, dexamethasone)
 count(oneline, ethnicity)
 oneline = oneline %>% 
   mutate(
-    ethnicity.4lev = fct_collapse(ethnicity,
+    ethnicity.4levels = fct_collapse(ethnicity,
                                   "Other Ethnic Minority" = 
                                     c("Other", "Arab", "Latin American", "Aboriginal/First Nations", "West Asian")) %>% 
-      fct_relevel("White", "South Asian", "East Asian", "Black", "Other Ethnic Minority"))
-count(oneline, ethnicity.4lev, ethnicity)
+      fct_relevel("White", "South Asian", "East Asian", "Black", "Other Ethnic Minority") %>% 
+      ff_label("Ethnicity"))
+count(oneline, ethnicity.4levels, ethnicity)
 
-# labeling ----
+
+# symptoms ----
+# Form symptom variable from existing symptom CRF
+count(oneline, fever_ceoccur_v2)
+oneline = oneline %>% 
+  mutate(across(fever_ceoccur_v2:anosmia_ceoccur_v2,
+                ~ fct_recode(.,
+                             "Yes" = "YES",
+                             "No" = "NO",
+                             "Unknown" = "Unknown"))
+  )
+# add in .names = "{.col}2" inside accross() to test that worked before over-writing
+# count(oneline, fever_ceoccur_v2, fever_ceoccur_v22)
+
 oneline = oneline %>% 
   mutate(
-    any_icu         = ff_label(any_icu, "ICU/HDU admission"),
-    any_invasive    = ff_label(any_invasive, "Invasive ventilation"),
+    number_symptoms = select(., fever_ceoccur_v2:anosmia_ceoccur_v2) %>% 
+      {. == "Yes"} %>% 
+      rowSums(na.rm = TRUE) %>% 
+      ff_label("Number of symptoms"),
+    
+    number_symptoms.factor = case_when(
+      number_symptoms < 1    ~ "0",
+      number_symptoms < 2    ~ "1", 
+      is.na(number_symptoms) ~ NA_character_,
+      TRUE                   ~ "2+") %>% 
+      factor() %>% 
+      ff_label("Number of symptoms"),
+    
+    any_symptoms = case_when(
+      number_symptoms == 0              ~ "No",
+      number_symptoms  > 0              ~ "Yes",
+      asymptomatic    == "Asymptomatic" ~ "No",
+      no_symptoms     == "YES"          ~ "No",
+      no_symptoms_v3  == "YES"          ~ "No",
+      adm_no_symp     == 1              ~ "No",
+      no_symptoms     == "No"           ~ "Yes",
+      no_symptoms_v3  == "No"           ~ "Yes"
+    ) %>% 
+      ff_label("Any symptoms")
+    )
+count(oneline, number_symptoms.factor, number_symptoms)
+count(oneline, any_symptoms, number_symptoms.factor)
+
+
+# labelling ----
+oneline = oneline %>% 
+  mutate(
+    any_icu         = ff_label(any_icu,         "ICU/HDU admission"),
+    any_invasive    = ff_label(any_invasive,    "Invasive ventilation"),
     any_noninvasive = ff_label(any_noninvasive, "Noninvasive ventillation")
   )
-
-
